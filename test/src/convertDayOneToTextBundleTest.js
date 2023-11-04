2a9502d1ed79e938713f0ee08796db23d0408659
Petteri Kamppuri <petteri.kamppuri@me.com>
2023-11-04 21:25:07 +0200

    test for textpack generation with pdf

--- test/src/convertDayOneToTextBundleTest.js
+++ test/src/convertDayOneToTextBundleTest.js
@@ -12,6 +12,7 @@ describe("convertDayOneToTextBundle", () => {
     it("creates files with proper names, timestamps and contents", async () => {
       // convert
       const targetDir = "./test/tmp/convertDayOneToTextBundle";
+      const referenceDir = __dirname + "/../assets/TextPacks";
       rimraf.sync(targetDir);
       fs.mkdirSync(targetDir);
       const logs = await convertDayOneToTextBundle(
@@ -22,20 +23,21 @@ describe("convertDayOneToTextBundle", () => {
       // check logs
       assert.deepEqual(logs, {
         converter: {
-          numberOfTextBundlesWritten: 3,
+          numberOfTextBundlesWritten: 4,
           filesErrors: [],
         },
         parser: {
           entriesErrors: [
             "Missing #text property 6BD0AE9E21C447A6BC504A63899BA544 0",
           ],
-          numberOfEntries: 3,
+          numberOfEntries: 4,
           numberOfEntriesWithErrors: 1,
         },
       });
 
       // files are created with proper filenames
       assert.deepEqual(fs.readdirSync(targetDir), [
+        "Fourth Test Journal Entry.DFE9217ECAD140A49006530651446773.textpack",
         "Initial entry.F4CF0509F3EA47D1B56F95D37F165F5E.textpack",
         "Second entry.6BD0AE9E21C447A6BC504A63899BA543.textpack",
         "Third entry with double escapes.6BD0AE9E21C447A6BC504A63899BC544.textpack",
@@ -72,6 +74,30 @@ describe("convertDayOneToTextBundle", () => {
         {
           birthtime: new Date("2010-02-26T21:44:20.000Z"),
           mtime: new Date("2010-03-26T21:48:55.000Z"),
+        }
+      );
+
+      assert.deepEqual(
+        statSubset(
+          fs.statSync(
+            `${targetDir}/Third entry with double escapes.6BD0AE9E21C447A6BC504A63899BC544.textpack`
+          )
+        ),
+        {
+          birthtime: new Date("2010-02-27T21:14:20.000Z"),
+          mtime: new Date("2010-03-27T21:58:55.000Z"),
+        }
+      );
+
+      assert.deepEqual(
+        statSubset(
+          fs.statSync(
+            `${targetDir}/Fourth Test Journal Entry.DFE9217ECAD140A49006530651446773.textpack`
+          )
+        ),
+        {
+          birthtime: new Date("2023-11-04T18:45:27.000Z"),
+          mtime: new Date("2023-11-04T18:45:44.000Z"),
         }
       );
 
@@ -125,6 +151,36 @@ some text
         `# Second entry
 
 some text`
+      );
+
+      // file4 content is correct
+      const zip4 = await JSZip.loadAsync(
+        fs.readFileSync(
+          `${targetDir}/Fourth Test Journal Entry.DFE9217ECAD140A49006530651446773.textpack`
+        )
+      );
+      assert.deepEqual(
+        await zip4.file("info.json").async("string"),
+        `{
+  "version": 2,
+  "type": "net.daringfireball.markdown",
+  "transient": true,
+  "creatorIdentifier": "org.dayoneexport.vendor",
+  "sourceURL": "dayone2://view?entryId=DFE9217ECAD140A49006530651446773"
+}`
+      );
+      assert.deepEqual(
+        await zip4.file("text.md").async("string"),
+        `Fourth Test Journal Entry
+This entry contains a PDF file.
+
+[0E4BD5DF4D8A4835B3BA3A53F31CE1AD.pdf](assets/2f1c651de5d167db3d62dc260925fa9c.pdf)<!-- {"embed":"true", "preview":"true"} -->
+
+Content continues here after the PDF.`
+      );
+      assert.equal(
+        await zip4.file("assets/2f1c651de5d167db3d62dc260925fa9c.pdf").name,
+        "assets/2f1c651de5d167db3d62dc260925fa9c.pdf"
       );
     });
   });
